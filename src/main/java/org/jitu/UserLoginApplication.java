a1bb3f6a2d8bde40496d1b1e298d01f02a2de6c7
Jittu <jeetkrkewat@gmail.com>
2020-02-11 21:59:52 +0530

    Initial Commit

---
+++ src/main/java/org/jitu/UserLoginApplication.java
@@ -0,0 +1,12 @@
+package org.jitu;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserLoginApplication.class, args);
+	}
+
+}
